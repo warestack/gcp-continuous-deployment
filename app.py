701bba98587639bacdfe4c41444b1393811b97f4
Dimitris Kargatzis <dkargatzis@gmail.com>
2023-04-18 22:08:15 +0300

    chore: Enhance readability by adding comments and adjusting the output message of the app
    Signed-off-by: Dimitris Kargatzis <dkargatzis@gmail.com>

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'Hello world! (v1)'
+    return 'Hello, this is the version `1.0.0` of your flask app'
 
-app.run(host='0.0.0.0', port=5000)
+
+# When you containerize your Flask app using Docker and expose it to the outside world, you need to specify the host
+# parameter in the app.run() function to make the app accessible from outside the container.
+
+# When you run a Flask app inside a Docker container, the default host value localhost refers only to the local
+# container, not to the outside world. To make a Flask app accessible from outside the container, you need to set the
+# host parameter to 0.0.0.0 so that it listens on all network interfaces.
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
